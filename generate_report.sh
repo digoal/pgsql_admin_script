d136780270171cb522558b021136ed2940c066ad
Digoal.zhou <digoal@126.com>
2016-06-09 09:50:17 +0800

    Update generate_report.sh

--- generate_report.sh
+++ generate_report.sh
@@ -229,7 +229,7 @@ echo "----->>>---->>>  获取postgresql.conf配置: "
 grep '^\ *[a-z]' $PGDATA/postgresql.conf|awk -F "#" '{print $1}'
 echo "建议: "
 echo "    主备配置尽量保持一致, 配置合理的参数值."
-echo -e "    建议修改的参数列表如下  ( 假设操作系统内存为128GB, 数据库独占操作系统, 数据库版本9.4.x )  : 
+echo -e "    建议修改的参数列表如下  ( 假设操作系统内存为128GB, 数据库独占操作系统, 数据库版本9.4.x, 其他版本可能略有不同, 未来再更新进来 )  : 
 echo ""
 listen_addresses = '0.0.0.0'            # 监听所有IPV4地址
 port = 1921                             # 监听非默认端口
@@ -240,7 +240,7 @@ unix_socket_permissions = 0700          # 确保权限安全
 tcp_keepalives_idle = 30                # 间歇性发送TCP心跳包, 防止连接被网络设备中断.
 tcp_keepalives_interval = 10
 tcp_keepalives_count = 10
-shared_buffers = 16GB                   # 数据库自己管理的共享内存大小
+shared_buffers = 16GB                   # 数据库自己管理的共享内存大小, 如果用大页, 建议设置为: 内存 - 100*work_mem - autovacuum_max_workers*(autovacuum_work_mem or autovacuum_work_mem) - max_connections*1MB
 huge_pages = try                        # 尽量使用大页, 需要操作系统支持, 配置vm.nr_hugepages*2MB大于shared_buffers.
 maintenance_work_mem = 512MB            # 可以加速创建索引, 回收垃圾(假设没有设置autovacuum_work_mem)
 autovacuum_work_mem = 512MB             # 可以加速回收垃圾
@@ -296,25 +296,37 @@ pg_stat_statements.track_utility=off
     建议的操作系统配置(根据实际情况修改) : 
 vi /etc/sysctl.conf
 # add by digoal.zhou
-kernel.shmmax = 135497418752
-kernel.shmmni = 4096
-fs.file-max = 7672460
-fs.aio-max-nr = 1048576
-vm.zone_reclaim_mode=0   # 禁用 numa, 或者在vmlinux中禁止.
-vm.swappiness = 0     #  关闭交换分区
-kernel.shmmax=135497418752     # 最大共享内存段大小
+kernel.shmmax = 135497418752     # 最大单个共享内存段大小
+kernel.shmmni = 8192
+kernel.sem = 51200 150994944 512000 51200     # 信号量, ipcs -l查看
+kernel.core_pattern = /data01/corefiles/core_%e_%u_%t_%s.%p
+net.ipv4.tcp_keepalive_intvl = 20
+net.ipv4.tcp_keepalive_probes = 6
+net.ipv4.tcp_keepalive_time = 60
+net.ipv4.tcp_synack_retries = 2
+net.ipv4.tcp_syncookies = 1   # 开启SYN Cookies。当出现SYN等待队列溢出时，启用cookie来处理，可防范少量的SYN攻击
+net.ipv4.tcp_tw_recycle = 1   # 开启TCP连接中TIME-WAIT套接字的快速回收
+net.ipv4.tcp_tw_reuse = 1     # 开启重用。允许将TIME-WAIT套接字重新用于新的TCP连接
+net.ipv4.tcp_timestamps = 1   # 减少time_wait
+net.ipv4.tcp_tw_timeout = 5   # 快速回收TIME_WAIT状态的TCP socket  https://en.wikipedia.org/wiki/Transmission_Control_Protocol
+net.ipv4.ip_local_port_range = 9000 65535    # 本地自动分配的TCP UDP端口号范围
 net.core.rmem_max = 4194304   # The maximum receive socket buffer size in bytes
 net.core.wmem_max = 4194304    # The maximum send socket buffer size in bytes.
 net.core.rmem_default = 262144   # The default setting of the socket receive buffer in bytes.
 net.core.wmem_default = 262144   # The default setting (in bytes) of the socket send buffer.
-net.ipv4.ip_local_port_range = 9000 65535    # 本地自动分配的TCP UDP端口号范围
-kernel.sem = 50100 64128000 50100 1280     # 信号量
-vm.dirty_background_bytes = 102400000      # 系统脏页到达这个值，系统后台刷脏页调度进程 pdflush（或其他） 自动将(dirty_expire_centisecs/100）秒前的脏页刷到磁盘
+fs.file-max = 7672460
+fs.aio-max-nr = 1048576
+vm.extra_free_kbytes = 512000
+vm.min_free_kbytes = 2097152
+vm.mmap_min_addr = 4096
+vm.zone_reclaim_mode = 0   # 禁用 numa, 或者在vmlinux中禁止.
+vm.swappiness = 0     #  关闭交换分区
+vm.dirty_background_bytes = 409600000      # 系统脏页到达这个值，系统后台刷脏页调度进程 pdflush（或其他） 自动将(dirty_expire_centisecs/100）秒前的脏页刷到磁盘
 vm.dirty_expire_centisecs = 6000    #  比这个值老的脏页，将被刷到磁盘。6000表示60秒。
 vm.dirty_writeback_centisecs = 50  # pdflush（或其他）后台刷脏页进程的唤醒间隔， 50表示0.5秒。
 vm.dirty_ratio = 80        #  如果系统进程刷脏页太慢，使得系统脏页超过内存 80 % 时，则用户进程如果有写磁盘的操作（如fsync, fdatasync等调用），则需要主动把系统脏页刷出。
 vm.nr_hugepages = 102352    #  大页数量，乘以/proc/meminfo Hugepagesize就是内存数量。
-vm.overcommit_memory = 2     #  在分配内存时，不允许over malloc
+vm.overcommit_memory = 0     #  在分配内存时，允许少量over malloc
 vm.overcommit_ratio = 90     #  当overcommit_memory = 2 时，用于参与计算允许指派的内存大小。
 
 vi /etc/security/limits.conf
