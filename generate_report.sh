3a1325591466423fd06aaf33844f09c08ba721fd
Digoal.zhou <digoal@126.com>
2017-05-21 19:44:49 +0800

    Update generate_report.sh

--- generate_report.sh
+++ generate_report.sh
@@ -942,14 +942,14 @@ t_wait as
 (  
   select a.mode,a.locktype,a.database,a.relation,a.page,a.tuple,a.classid,a.granted, 
   a.objid,a.objsubid,a.pid,a.virtualtransaction,a.virtualxid,a.transactionid,a.fastpath,  
-  b.state,b.query,b.xact_start,b.query_start,b.usename,b.datname 
+  b.state,b.query,b.xact_start,b.query_start,b.usename,b.datname,b.client_addr,b.client_port,b.application_name 
     from pg_locks a,pg_stat_activity b where a.pid=b.pid and not a.granted 
 ), 
 t_run as 
 ( 
   select a.mode,a.locktype,a.database,a.relation,a.page,a.tuple,a.classid,a.granted, 
   a.objid,a.objsubid,a.pid,a.virtualtransaction,a.virtualxid,a.transactionid,a.fastpath, 
-  b.state,b.query,b.xact_start,b.query_start,b.usename,b.datname 
+  b.state,b.query,b.xact_start,b.query_start,b.usename,b.datname,b.client_addr,b.client_port,b.application_name 
     from pg_locks a,pg_stat_activity b where a.pid=b.pid and a.granted 
 ), 
 t_overlap as 
@@ -977,10 +977,11 @@ t_unionall as
 )  
 select locktype,datname,relation::regclass,page,tuple,virtualxid,transactionid::text,classid::regclass,objid,objsubid, 
 string_agg( 
-'Pid: '||pid||chr(10)|| 
-'Granted: '||granted||' , Mode: '||mode||' , FastPath: '||fastpath||' , VirtualTransaction: '||virtualtransaction||' , Session_State: '||state||chr(10)|| 
-'Username: '||usename||' , Database: '||datname||' , Xact_Start: '||xact_start||' , Query_Start: '||query_start||' , Xact_Elapse: '||now()-xact_start||' , Query_Elapse: '||now()-query_start||chr(10)||  
-'Query: '||query,  
+'Pid: '||case when pid is null then 'NULL' else pid::text end||chr(10)|| 
+'Granted: '||case when granted is null then 'NULL' else granted::text end||' , Mode: '||case when mode is null then 'NULL' else mode::text end||' , FastPath: '||case when fastpath is null then 'NULL' else fastpath::text end||' , VirtualTransaction: '||case when virtualtransaction is null then 'NULL' else virtualtransaction::text end||' , Session_State: '||case when state is null then 'NULL' else state::text end||chr(10)|| 
+'Username: '||case when usename is null then 'NULL' else usename::text end||' , Database: '||case when datname is null then 'NULL' else datname::text end||' , Client_Addr: '||case when client_addr is null then 'NULL' else client_addr::text end||' , Client_Port: '||case when client_port is null then 'NULL' else client_port::text end||' , Application_Name: '||case when application_name is null then 'NULL' else application_name::text end||chr(10)||  
+'Xact_Start: '||case when xact_start is null then 'NULL' else xact_start::text end||' , Query_Start: '||case when query_start is null then 'NULL' else query_start::text end||' , Xact_Elapse: '||case when (now()-xact_start) is null then 'NULL' else (now()-xact_start)::text end||' , Query_Elapse: '||case when (now()-query_start) is null then 'NULL' else (now()-query_start)::text end||chr(10)||  
+'Query: '||case when query is null then 'NULL' else query::text end,  
 chr(10)||'--------'||chr(10)  
 order by  
   (  case mode  
